44f0c2460e6a03e23d3b9a8d3d3741d1a50de394
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2026-03-20 15:08:23 +0530

    Initial Spring Boot commit

---
+++ src/main/java/com/example/springboot_otp_backend/SpringbootOtpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_otp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOtpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOtpBackendApplication.class, args);
+	}
+
+}
