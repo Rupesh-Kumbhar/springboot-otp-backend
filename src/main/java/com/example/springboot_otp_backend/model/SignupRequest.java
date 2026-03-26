9e549044a1459d74aed81e0812a4aca009fcb378
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2026-03-26 23:35:05 +0530

    DB connetion,login signup Model Added

---
+++ src/main/java/com/example/springboot_otp_backend/model/SignupRequest.java
@@ -0,0 +1,13 @@
+package com.example.springboot_otp_backend.model;
+
+// SignupRequest.java
+public class SignupRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String confirmPassword;
+    private String phone;
+    private String role; // "USER" or "ADMIN"
+    // getters/setters
+}
---
+++ src/main/java/com/example/springboot_otp_backend/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.springboot_otp_backend.repository;
+
+
+
+import com.example.springboot_otp_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByPhone(String phone);
+}
